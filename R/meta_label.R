2a501d744e2f89318527e57580b758811dfe06bc
bradyrippon <brady.rippon@gmail.com>
2025-08-12 17:58:25 -0400

    added functionality to handle data values

--- R/meta_label.R
+++ R/meta_label.R
@@ -7,7 +7,9 @@
 #'
 #' @param data A data frame you want to label.
 #' @param yaml_path Path to YAML file containing variable labels.
-#' @param warn_missing Logical, controls warnings for metadata variables not found in data frame.
+#' @param warn_missing Logical; controls warnings for metadata variables not found in data frame.
+#' @param values Character (none, overwrite, new); controls how to handle factor values.
+#' @param values_tag Character, string for variable suffix when values = "new."
 #'
 #' @return Original data frame with labels attached
 #' @export
@@ -21,21 +23,33 @@
 #' df <- data.frame(
 #'   age = c(23, 25, 42, 38, 29),
 #'   gender = c(1, 2, 1, 1, 2),
-#'   education = c(2, 3, 3, 5, 4)
+#'   education = c(2, 3, 3, 5, 4),
+#'   diabetes = c("N", "N", "2", "1", "N")
 #' )
 #'
-#' labeled_df <- meta_label(df, yaml_path)
-#' labelled::var_label(labeled_df$age)
+#' labels <- df |> meta_label(yaml_path, values = "overwrite")
 #' }
 
-meta_label <- function(data, yaml_path, warn_missing = FALSE) {
+meta_label <- function(
+    data,
+    yaml_path,
+    warn_missing = FALSE,
+    values = c("none", "overwrite", "new"),
+    values_tag = "_factor"
+  ) {
 
   ## initial errors from user inputs ----------
+
+  values <- match.arg(values) # validates argument
+
   if (!inherits(data, "data.frame")) {
     stop("`data` must be a data.frame or tibble")
   }
   if (!file.exists(yaml_path)) {
     stop("Specified metadata file does not exist: ", yaml_path)
+  }
+  if (!is.character(values_tag) || length(values_tag) != 1) {
+    stop("`values_tag` must be a single string")
   }
 
   # load metadata
@@ -44,6 +58,9 @@ meta_label <- function(data, yaml_path, warn_missing = FALSE) {
   if (!"labels" %in% names(metadata)) {
     stop("No labels found in metadata")
   }
+
+
+  ## labels ----------
 
   # check what variables in metadata were found in data frame
   missing_vars <- setdiff(names(metadata$labels), names(data))
@@ -57,6 +74,37 @@ meta_label <- function(data, yaml_path, warn_missing = FALSE) {
   # apply variable labels
   for (var in intersect(names(metadata$labels), names(data))) {
     labelled::var_label(data[[var]]) <- metadata$labels[[var]]
+  }
+
+
+  ## levels ----------
+
+  # apply variable levels (optional, based on user preference)
+  if (values != "none" && "values" %in% names(metadata)) {
+    for (var in intersect(names(metadata$values), names(data))) {
+      val_map <- metadata$values[[var]]
+
+      # match as character (avoids type mismatch)
+      var_factor <- factor(
+        as.character(data[[var]]),
+        levels = names(val_map),
+        labels = unname(val_map)
+      )
+
+      if (values == "overwrite") {
+        data[[var]] <- var_factor
+        if (var %in% names(metadata$labels)) { # keep original label
+          labelled::var_label(data[[var]]) <- metadata$labels[[var]]
+        }
+
+      } else if (values == "new") {
+        var_new <- paste0(var, values_tag)
+        data[[var_new]] <- var_factor
+        if (var %in% names(metadata$labels)) { # keep original label
+          labelled::var_label(data[[var_new]]) <- metadata$labels[[var]]
+        }
+      }
+    }
   }
 
   return(data)
--- tests/testthat/test-meta_label.R
+++ tests/testthat/test-meta_label.R
@@ -25,19 +25,60 @@ test_that("meta_label applies labels correctly", {
 
   yaml_path <- system.file("extdata", "codebook.yaml", package = "wcmtools")
   df <- data.frame(
-    age = c(23, 25, 42),
-    gender = c(1, 2, 1),
-    education = c(2, 3, 3)
+    age = c(23, 25, 42, 38, 29),
+    gender = c(1, 2, 1, 1, 2),
+    education = c(2, 3, 3, 5, 4),
+    diabetes = c("N", "N", "2", "1", "N"),
+    dx = c(1, 0, 0, 0, 1)
   ) |>
     meta_label(yaml_path)
 
   # labels applied correctly
   expect_equal(var_label(df$age), "Age (y)")
-  expect_equal(var_label(df$gender), "Sex")
+  expect_equal(var_label(df$education), "Highest level of education")
 
   # NULL label for unnamed variables
-  expect_null(var_label(df$notthere))
+  expect_null(var_label(df$dx))
 })
+
+
+test_that("meta_label leaves variables unchanged when values = none", {
+  yaml_path <- system.file("extdata", "codebook.yaml", package = "wcmtools")
+  df <- data.frame(gender = c(2, 1)) |>
+    meta_label(yaml_path, values = "none")
+
+  expect_false(is.factor(df$gender))
+  expect_equal(var_label(df$gender), "Sex")
+})
+
+
+test_that("meta_label applies value labels with overwrite", {
+
+  yaml_path <- system.file("extdata", "codebook.yaml", package = "wcmtools")
+  df <- data.frame(gender = c(2, 1)) |>
+    meta_label(yaml_path, values = "overwrite")
+
+  expect_s3_class(df$gender, "factor")
+  expect_equal(levels(df$gender), c("Male", "Female", "Other"))
+  expect_equal(as.character(df$gender), c("Female", "Male"))
+  expect_equal(var_label(df$gender), "Sex")
+})
+
+
+test_that("meta_label applies value labels with new factor and custom suffix", {
+
+  yaml_path <- system.file("extdata", "codebook.yaml", package = "wcmtools")
+  df <- data.frame(gender = c(2, 1)) |>
+    meta_label(yaml_path, values = "new", values_tag = "_cats")
+
+  expect_false("gender" %in% names(df) && is.factor(df$gender))
+  expect_true("gender_cats" %in% names(df))
+  expect_s3_class(df$gender_cats, "factor")
+  expect_equal(levels(df$gender_cats), c("Male", "Female", "Other"))
+  expect_equal(var_label(df$gender_cats), "Sex")
+  expect_equal(as.character(df$gender_cats), c("Female", "Male"))
+})
+
 
 
 # ---------- Errors ----------
@@ -60,6 +101,7 @@ test_that("meta_label errors for invalid inputs", {
   writeLines("foo: bar", tmp_yaml)
   expect_error(df |> meta_label(tmp_yaml))
 })
+
 
 
 # ---------- Warnings ----------
